2fc3d290d50ee1ee4f8d771f1784cc326bf6ee9b
cs-oak <csharma@g.clemson.edu>
2019-06-28 23:45:37 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Navigation} from './components/navigation'
 import './App.css';
 
 function App() {
+  const headerBrand = 'Steam Stats App';
+  const themes = ['Light', 'Dark']
+  let state = {
+      theme: 'dark'
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Navigation headerBrand={headerBrand} themes={themes} defaultTheme={state.theme}/>
   );
 }
 
---
+++ src/components/navigation.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import {Navbar, Dropdown} from 'react-bootstrap'
+
+export class Navigation extends Component {
+    render() {
+        return(
+            <Navbar bg={this.props.defaultTheme} variant={this.props.defaultTheme} expand={'lg'}>
+                <Navbar.Brand>
+                    {this.props.headerBrand}
+                </Navbar.Brand>
+                <Dropdown>
+                    <Dropdown.Toggle>Themes</Dropdown.Toggle>
+                    <Dropdown.Menu>
+                        {this.props.themes.map((theme) => <Dropdown.Item>{theme}</Dropdown.Item>)}
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Navbar>
+        );
+    }
+}
